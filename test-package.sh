96a0f924b95cc431ebefaea459c5b3eea02b1300
chlablak <chlablak@gmail.com>
2014-04-18 14:06:46 +0200

    Test OK

--- test-package.sh
+++ test-package.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 . ./make-package.sh
-sudo apt-get purge $SRC
+sudo rm -r "/etc/$SRC/"
+sudo rm -r "/var/log/$SRC/"
+sudo rm -r "/usr/bin/$SRC/"
+sudo apt-get remove $SRC -y
 sudo dpkg -i "$DST/$SRC.deb"
 
